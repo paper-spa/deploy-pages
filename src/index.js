574c854d6e3db8e5bb05054a6d19dc887582bb5f
Tommy Byrd <tcbyrd@github.com>
2021-11-17 16:23:35 -0500

    refactor and add tests

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
+require('regenerator-runtime/runtime')
+
 // This package assumes a site has already been built and the files exist in the current workspace
 // If there's an artifact named `artifact.tar`, it can upload that to actions on its own,
 // without the user having to do the tar process themselves.
@@ -7,108 +9,115 @@ const core = require('@actions/core')
 const axios = require('axios')
 
 // All variables we need from the runtime are loaded here
-const context = require('./context')
+const getContext = require('./context')
 
 // TODO: If the artifact hasn't been created, we can create it and upload to artifact storage ourselves
 // const tar = require('tar')
-
-// Ask the runtime for the unsigned artifact URL and deploy to GitHub Pages
-// by creating a deployment with that artifact
-async function create() {
-  try {
-    core.info(`Actor: ${context.buildActor}`)
-    core.info(`Action ID: ${context.actionsId}`)
-    const pagesDeployEndpoint = `https://api.github.com/repos/${context.repositoryNwo}/pages/deployment`
-    const artifactExgUrl = `${context.runTimeUrl}_apis/pipelines/workflows/${context.workflowRun}/artifacts?api-version=6.0-preview`
-    core.info(`Artifact URL: ${artifactExgUrl}`)
-    const {data} = await axios.get(artifactExgUrl, {
-      headers: {
-        Authorization: `Bearer ${context.runTimeToken}`,
-        'Content-Type': 'application/json'
-      }
-    })
-    core.info(JSON.stringify(data))
-    const artifactUrl = `${data.value[0].url}&%24expand=SignedContent`
-    const response = await axios.post(
-      pagesDeployEndpoint,
-      {artifact_url: artifactUrl, pages_build_version: context.buildVersion},
-      {
-        headers: {
-          Accept: 'application/vnd.github.v3+json',
-          Authorization: `Bearer ${context.githubToken}`,
-          'Content-type': 'application/json'
-        }
-      }
-    )
-    core.info(`Created deployment for ${context.buildVersion}`)
-    core.info(JSON.stringify(response.data))
-  } catch (error) {
-    core.info('Failed to create deployment.')
-    core.setFailed(error)
+class Deployment {
+  constructor() {
+    const context = getContext()
+    this.runTimeUrl = context.runTimeUrl
+    this.repositoryNwo = context.repositoryNwo
+    this.runTimeToken = context.runTimeToken
+    this.buildVersion = context.buildVersion
+    this.buildActor = context.buildActor
+    this.actionsId = context.workflowRun
+    this.githubToken = context.githubToken
+    this.workflowRun = context.workflowRun
   }
-}
 
-// Poll the deployment endpoint for status
-async function check() {
-  try {
-    const statusUrl = `https://api.github.com/repos/${context.repositoryNwo}/pages/deployment/status/${process.env['GITHUB_SHA']}`
-    const timeout = core.getInput('timeout')
-    const timeout_duration = core.getInput('timeout_duration')
-    const error_count_max = core.getInput('error_count')
-    var tries = 0
-    var error_count = 0
-    while (tries < timeout) {
-      tries++
-      await new Promise(r => setTimeout(r, timeout_duration))
-      var res = await axios.get(statusUrl, {
+  // Ask the runtime for the unsigned artifact URL and deploy to GitHub Pages
+  // by creating a deployment with that artifact
+  async create() {
+    try {
+      core.info(`Actor: ${this.buildActor}`)
+      core.info(`Action ID: ${this.actionsId}`)
+      const pagesDeployEndpoint = `https://api.github.com/repos/${this.repositoryNwo}/pages/deployment`
+      const artifactExgUrl = `${this.runTimeUrl}_apis/pipelines/workflows/${this.workflowRun}/artifacts?api-version=6.0-preview`
+      core.info(`Artifact URL: ${artifactExgUrl}`)
+      const {data} = await axios.get(artifactExgUrl, {
         headers: {
-          Authorization: `token ${context.githubToken}`
+          Authorization: `Bearer ${this.runTimeToken}`,
+          'Content-Type': 'application/json'
         }
       })
-
-      if (res.data.status == 'succeed') {
-        core.info('Reported success!')
-        core.setOutput('status', 'succeed')
-        break
-      } else {
-        core.info('Current status: ' + res.data.status)
-      }
-
-      if (res.status != 200) {
-        error_count++
-      }
-
-      if (error_count >= error_count_max) {
-        core.info('Too many errors, aborting!')
-        core.setFailed('Failed with status code: ' + res.status)
-        break
-      }
+      core.info(JSON.stringify(data))
+      const artifactUrl = `${data.value[0].url}&%24expand=SignedContent`
+      const response = await axios.post(
+        pagesDeployEndpoint,
+        {artifact_url: artifactUrl, pages_build_version: this.buildVersion},
+        {
+          headers: {
+            Accept: 'application/vnd.github.v3+json',
+            Authorization: `Bearer ${this.githubToken}`,
+            'Content-type': 'application/json'
+          }
+        }
+      )
+      core.info(`Created deployment for ${this.buildVersion}`)
+      core.info(JSON.stringify(response.data))
+    } catch (error) {
+      core.info(`Failed to create deployment for ${this.buildVersion}.`)
+      core.setFailed(error)
     }
-    if (tries >= timeout) {
-      core.info('Timeout reached, aborting!')
-      core.setFailed('Timeout reached, aborting!')
+  }
+
+  // Poll the deployment endpoint for status
+  async check() {
+    try {
+      const statusUrl = `https://api.github.com/repos/${this.repositoryNwo}/pages/deployment/status/${this.process.env['GITHUB_SHA']}`
+      const timeout = core.getInput('timeout')
+      const timeout_duration = core.getInput('timeout_duration')
+      const error_count_max = core.getInput('error_count')
+      var tries = 0
+      var error_count = 0
+      while (tries < timeout) {
+        tries++
+        await new Promise(r => setTimeout(r, timeout_duration))
+        var res = await axios.get(statusUrl, {
+          headers: {
+            Authorization: `token ${this.githubToken}`
+          }
+        })
+
+        if (res.data.status == 'succeed') {
+          core.info('Reported success!')
+          core.setOutput('status', 'succeed')
+          break
+        } else {
+          core.info('Current status: ' + res.data.status)
+        }
+
+        if (res.status != 200) {
+          error_count++
+        }
+
+        if (error_count >= error_count_max) {
+          core.info('Too many errors, aborting!')
+          core.setFailed('Failed with status code: ' + res.status)
+          break
+        }
+      }
+      if (tries >= timeout) {
+        core.info('Timeout reached, aborting!')
+        core.setFailed('Timeout reached, aborting!')
+      }
+    } catch (error) {
+      core.setFailed(error)
     }
-  } catch (error) {
-    core.setFailed(error)
   }
 }
 
-function ensureContext() {
-  for (const variable in context) {
-    if (context[variable] === undefined) {
-      throw new Error(`${variable} is undefined. Cannot continue.`)
-    }
-  }
-  core.debug('all variables are set')
-}
 async function main() {
   try {
-    ensureContext()
-    await create()
-    await check()
+    const deployment = new Deployment()
+    await deployment.create()
+    await deployment.check()
   } catch (error) {
     core.setFailed(error)
   }
 }
 
 main()
+
+module.exports = Deployment
