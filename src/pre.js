d66bebf5eddf6eb4930c989e5118104c9e148eb0
yimysty <yimysty@github.com>
2021-11-23 17:37:07 -0800

    added retry~

--- src/pre.js
+++ src/pre.js
@@ -1,5 +1,20 @@
 const core = require('@actions/core')
 const axios = require('axios')
+const axiosRetry = require('axios-retry');
+const retryAttempt = 3
+
+axiosRetry(axios, {
+  retries: retryAttempt,
+  retryDelay: (retryCount) => {
+    core.info(`retrying to send pages telemetry with attempt: ${retryCount}`)
+    return retryCount * 1000; // time interval between retries, with 1s, 2s, 3s
+  },
+
+  // retry on error greater than 500
+  retryCondition: (error) => {
+    return error.response.status >= 500;
+  },
+});
 
 // All variables we need from the runtime are loaded here
 const context = require('./context')
@@ -26,7 +41,11 @@ async function emitTelemetry(){
         'Content-type': 'application/json'
       }
     }
-  )
+  ).catch((err) => {
+    if (err.response.status !== 200) {
+      throw new Error(`failed to emit metric with status code: ${err.response.status} after ${retryAttempt} retry attempts`);
+    }
+  });
 }
 
 async function main() {
