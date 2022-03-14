c2dba3e29194b6d2cf6d5ed1b37282e11e1959cc
James M. Greene <JamesMGreene@github.com>
2022-03-14 12:19:06 -0500

    Mock 'preview' getInput using env vars

--- src/index.test.js
+++ src/index.test.js
@@ -70,6 +70,11 @@ describe('create', () => {
     jest.spyOn(core, 'debug').mockImplementation(jest.fn())
   })
 
+  afterEach(() => {
+    // Remove mock for `core.getInput('preview')`
+    delete process.env.INPUT_PREVIEW
+  })
+
   it('can successfully create a deployment', async () => {
     process.env.GITHUB_SHA = 'valid-build-version'
     const fakeJwt = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiJiNjllMWIxOC1jOGFiLTRhZGQtOGYxOC03MzVlMzVjZGJhZjAiLCJzdWIiOiJyZXBvOnBhcGVyLXNwYS9taW55aTplbnZpcm9ubWVudDpQcm9kdWN0aW9uIiwiYXVkIjoiaHR0cHM6Ly9naXRodWIuY29tL3BhcGVyLXNwYSIsInJlZiI6InJlZnMvaGVhZHMvbWFpbiIsInNoYSI6ImEyODU1MWJmODdiZDk3NTFiMzdiMmM0YjM3M2MxZjU3NjFmYWM2MjYiLCJyZXBvc2l0b3J5IjoicGFwZXItc3BhL21pbnlpIiwicmVwb3NpdG9yeV9vd25lciI6InBhcGVyLXNwYSIsInJ1bl9pZCI6IjE1NDY0NTkzNjQiLCJydW5fbnVtYmVyIjoiMzQiLCJydW5fYXR0ZW1wdCI6IjIiLCJhY3RvciI6IllpTXlzdHkiLCJ3b3JrZmxvdyI6IkNJIiwiaGVhZF9yZWYiOiIiLCJiYXNlX3JlZiI6IiIsImV2ZW50X25hbWUiOiJwdXNoIiwicmVmX3R5cGUiOiJicmFuY2giLCJlbnZpcm9ubWVudCI6IlByb2R1Y3Rpb24iLCJqb2Jfd29ya2Zsb3dfcmVmIjoicGFwZXItc3BhL21pbnlpLy5naXRodWIvd29ya2Zsb3dzL2JsYW5rLnltbEByZWZzL2hlYWRzL21haW4iLCJpc3MiOiJodHRwczovL3Rva2VuLmFjdGlvbnMuZ2l0aHVidXNlcmNvbnRlbnQuY29tIiwibmJmIjoxNjM4ODI4MDI4LCJleHAiOjE2Mzg4Mjg5MjgsImlhdCI6MTYzODgyODYyOH0.1wyupfxu1HGoTyIqatYg0hIxy2-0bMO-yVlmLSMuu2w'
@@ -119,8 +124,7 @@ describe('create', () => {
     axios.post = jest.fn().mockResolvedValue('test')
 
     // Return `"true"` for `core.getInput("preview")`
-    core.GetInput = jest.fn(input => { if (input === 'preview') return 'true' })
-    jest.spyOn(core, 'getInput')
+    process.env[`INPUT_${'preview'.toUpperCase()}`] = 'true'
 
     // Create the deployment
     const deployment = new Deployment()
