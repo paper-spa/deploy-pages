8167293bce584baed8bb54540705eb50351f9d31
James M. Greene <JamesMGreene@github.com>
2022-04-19 23:56:29 -0500

    Temporarily support API responses with preview_url values with and without the https scheme included

--- src/deployment.js
+++ src/deployment.js
@@ -79,8 +79,10 @@ class Deployment {
         const statusUrl = this.deploymentInfo != null ?
           this.deploymentInfo["status_url"] : `https://api.github.com/repos/${this.repositoryNwo}/pages/deployment/status/${this.buildVersion}`
         var page_url = this.deploymentInfo != null ? this.deploymentInfo["page_url"] : ""
-        if (this.deploymentInfo != null && this.deploymentInfo["preview_url"] != "") {
-          page_url = `https://${this.deploymentInfo["preview_url"]}`
+        const previewUrl = this.deploymentInfo != null ? this.deploymentInfo["preview_url"] : ""
+        if (this.isPreview && previewUrl) {
+          const urlPrefix = previewUrl.test(/^https?:\/\//) ? "" : "https://"
+          page_url = `${urlPrefix}${previewUrl}`
         }
         core.setOutput('page_url', page_url)
         const timeout = core.getInput('timeout')
